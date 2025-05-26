5db535466662ba0bdb9169e36e75f53375a2257d
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-26 19:41:20 +0200

    Modelo Peticion

---
+++ backend/models/Order.py
@@ -0,0 +1,31 @@
+# models/Order.py
+
+from backend import db
+from sqlalchemy.ext.hybrid import hybrid_property
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    total = db.Column(db.Numeric(10,2))
+    status = db.Column(db.String(20), default='pending')
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    items = db.relationship('OrderItem', backref='order', lazy=True)
+
+    @hybrid_property
+    def total_price(self):
+        return sum(item.subtotal for item in self.items)
+
+class OrderItem(db.Model):
+    __tablename__ = 'order_items'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price_at_purchase = db.Column(db.Numeric(10,2), nullable=False)
+    
+    @hybrid_property
+    def subtotal(self):
+        return self.price_at_purchase * self.quantity
