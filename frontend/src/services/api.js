dcb19a5799ce885d45adbd2bbfa54a6f08728228
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-27 16:54:50 +0200

    Servicio base de API

---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+// src/services/api.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true, // Para cookies JWT httpOnly
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response && err.response.status === 401) {
+      // Manejo global de auth
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
