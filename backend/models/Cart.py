01bff3b1ce7f7305c8bd4419992000d7f27883ba
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-27 16:48:49 +0200

    Crear modelos de carrito

---
+++ backend/models/Cart.py
@@ -0,0 +1,13 @@
+# models/Cart.py
+class Cart(db.Model):
+    __tablename__ = 'carts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
+    items = db.relationship('CartItem', backref='cart', cascade='all, delete-orphan')
+
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    quantity = db.Column(db.Integer, nullable=False)
