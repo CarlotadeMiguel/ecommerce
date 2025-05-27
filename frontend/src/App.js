f1869eece418898b4b01c46ba5740a54cbc5e4fa
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-27 17:09:15 +0200

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+// Importa aquí otras páginas (HomePage, CartPage, etc.)
 
 function App() {
   return (
-    <div class="bg-white p-8 rounded-lg shadow-lg max-w-md text-center">
-    <h1 class="text-3xl font-bold mb-4 text-blue-600">¡Hola, Tailwind!</h1>
-    <p class="text-gray-700 mb-6">Este es un ejemplo básico</p>
-  </div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+
+      </Routes>
+
+    </Router>
   );
 }
 
