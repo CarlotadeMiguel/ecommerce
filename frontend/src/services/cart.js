ba8e3084d06b5a01fc35186567dd5e1a92bf9e70
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-28 20:41:57 +0200

    comentar servicio de cart

--- frontend/src/services/cart.js
+++ frontend/src/services/cart.js
@@ -1,18 +1,29 @@
-// src/services/cart.js
-import api from "./api";
+// // src/services/cart.js
+// import api from "./api";
 
-export const addToCart = async (productId, quantity = 1) => {
-  try {
-    const response = await api.post("/cart/add", {
-      product_id: productId,
-      quantity: quantity
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("accessToken");
-      window.location = "/login";
-    }
-    throw error;
-  }
-};
+// export const getCart = async () => {
+//   try {
+//     const response = await api.get("/cart");
+//     return response.data;
+//   } catch {
+//     return { items: [] };
+//   }
+// };
+
+// export const addToCart = async (productId, quantity = 1) => {
+//   try {
+//     console.log("Adding to cart:", productId, quantity);
+//     const response = await api.post("/cart/add", {
+//       product_id: productId,
+//       quantity: quantity
+//     });
+//     return response.data;
+//   } catch (error) {
+//     if (error.response?.status === 401) {
+//       console.log(error.response)
+//       // localStorage.removeItem("accessToken");
+//       // window.location = "/login";
+//     }
+//     throw error;
+//   }
+// };
