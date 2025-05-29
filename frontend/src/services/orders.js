834f0a3c98198f7d268bf743c24734a5d1211b8e
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-29 18:07:52 +0200

    Order Service

--- frontend/src/services/orders.js
+++ frontend/src/services/orders.js
@@ -1,5 +1,21 @@
+// frontend/src/services/orders.js
 import api from './api';
 
-export const createOrder = (items) => api.post('/orders', { items });
-export const getOrders = () => api.get('/orders');
-export const getOrder = (id) => api.get(`/orders/${id}`);
+export const createOrder = async (items) => {
+    console.log(items);
+  try {
+    const response = await api.post('/orders/', items );
+
+    return response.data;
+  } catch (error) {
+    if (error.response?.data?.error === 'Stock insuficiente') {
+      throw new Error(`Productos sin stock: ${error.response.data.products.join(', ')}`);
+    }
+    throw new Error('Error al crear la orden');
+  }
+};
+
+export const getOrders = async () => {
+  const response = await api.get('/orders/');
+  return response.data;
+};
